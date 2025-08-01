560fb03b19721d56339ecf82f030c0791a554aa8
JacobCYShin <scey26@gmail.com>
2025-08-01 16:03:17 +0900

    add model_downloader.py again

---
+++ model_downloader.py
@@ -0,0 +1,7 @@
+from huggingface_hub import snapshot_download
+
+snapshot_download(
+    repo_id="black-forest-labs/FLUX.1-Fill-dev",
+    local_dir="models/flux-fill",
+    local_dir_use_symlinks=False
+)
